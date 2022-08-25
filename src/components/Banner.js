7da5317e7cef51eafce3c33f3dd28ccbbc1bb60a
villalbajoaquin <villalbajoaq@gmail.com>
2022-08-25 03:40:31 -0300

    feat: first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Banner(){
+    return (
+        <section className="banner">
+            <h1 className="titulo">Tenemos lo mejor para ti...</h1>
+        </section>
+    )
+}
+export default Banner;
---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from '../assets/img/logo-DH.png';
+
+function Footer(){
+    return (
+        <footer  className="pie">
+            <nav id="logo">
+                <a href="#"><img src={logo} alt="Logo Digital House"/></a>
+            </nav>
+            <nav id="opciones">
+                <ul>
+                    <li><a href="#">facebook</a></li>
+                    <li><a href="#">Instagram</a></li>
+                </ul>
+            </nav>
+        </footer>
+    )
+}
+export default Footer;
